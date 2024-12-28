1529a63e35cdd93f19ce5ba0bfb988e30fedddb1
Justin Law <justin.law@tesco.com>
2024-12-28 17:20:32 +0000

    basic setup

--- src/main/java/com/gubu/buffer/Main.java
+++
@@ -1,7 +0,0 @@
-package com.gubu.buffer;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/main/java/com/gubu/buffer/application/Buffer.java
@@ -0,0 +1,11 @@
+package com.gubu.buffer.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buffer {
+    public static void main(String[] args) {
+        SpringApplication.run(Buffer.class, args);
+    }
+}
