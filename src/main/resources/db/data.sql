acacb88814aa0b712c66cf2ec8dfa74c0dda4d8e
Justin Law <justin.law@tesco.com>
2025-01-03 22:44:18 +0000

    configure dummy data inserts

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO product_table (id, name) VALUES (1, 'Dummy 1');
+INSERT INTO product_table (id, name) VALUES (2, 'Dummy 2');
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE product_table {
-    id PRIMARY KEY,
+CREATE TABLE product_table (
+    id BIGINT PRIMARY KEY,
     name VARCHAR(500) NOT NULL
-}
+);
