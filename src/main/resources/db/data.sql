947bb431a0f5aeefa4cf8353e66be8ec9ecbbba2
Justin Law <justin.law@tesco.com>
2025-01-06 22:53:30 +0000

    fix data.sql - should not include id value if it's bigserial

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO product_table (id, name) VALUES (1, 'Dummy 1');
-INSERT INTO product_table (id, name) VALUES (2, 'Dummy 2');
+INSERT INTO product_table (name) VALUES ('Dummy 1');
+INSERT INTO product_table (name) VALUES ('Dummy 2');
