a67aa8c20b881df5580df129542508f4c9f53a77
Justin Law <justin.law@tesco.com>
2025-01-12 12:08:35 +0000

    unused import

--- src/main/java/com/gubu/buffer/application/ProductController.java
+++ src/main/java/com/gubu/buffer/application/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 import static com.gubu.buffer.domain.product.ProductMapper.toResponse;
 
