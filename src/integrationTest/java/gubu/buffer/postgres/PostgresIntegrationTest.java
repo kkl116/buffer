e9dafba13891f3027db3287e9ebda5cdd3df9825
Justin Law <justin.law@tesco.com>
2025-01-14 11:30:47 +0000

    add update product cost test

--- src/integrationTest/java/gubu/buffer/postgres/PostgresIntegrationTest.java
+++ src/integrationTest/java/gubu/buffer/postgres/PostgresIntegrationTest.java
@@ -8,7 +8,6 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Transactional
@@ -17,36 +16,51 @@ public class PostgresIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private ProductService productService;
 
+    private static final ProductEntity PRODUCT_1 = ProductEntity.builder()
+        .name("Product 1")
+        .build();
+
     @Test
     void shouldAddProductCostSuccessfully() {
         //Given
-        var productId = 1L;
-
-        productRepository.save(
-            ProductEntity.builder()
-            .name("Product 1")
-            .build()
-        );
+        productRepository.save(PRODUCT_1);
 
         var productCostRequestDto = new ProductCostRequestDto("paper", 20.00);
         //When
-        var returnedProductCost = productService.addProductCost(productId, productCostRequestDto);
+        var returnedProductCost = productService.addProductCost(1L, productCostRequestDto);
 
         //Then
-        //product should contain new cost entity
-        var fetchedProduct = productRepository.findById(productId);
-        assertThat(fetchedProduct.isPresent()).isTrue();
+        var fetchedProduct = productRepository.findById(1L);
         var productCost = fetchedProduct.get().getProductCosts().getFirst();
         assertEquals("paper", productCost.getName());
         assertEquals(20.00, productCost.getPrice());
 
-        //check cost should contain product entity
         var fetchedProductCost = productCostRepository.findById(productCost.getId());
-        assertThat(fetchedProductCost.isPresent()).isTrue();
-        assertEquals(productId, fetchedProductCost.get().getProduct().getId());
+        assertEquals(1L, fetchedProductCost.get().getProduct().getId());
         assertEquals(1L, fetchedProductCost.get().getId());
 
         //returnedProductCost should have the generated id
         assertEquals(1L, returnedProductCost.getId());
+    }
+
+    @Test
+    void shouldUpdateProductCostSuccessfully() {
+        //Given
+        productRepository.save(PRODUCT_1);
+        var savedProductCostRequestDto = new ProductCostRequestDto("old-paper", 10.00);
+        productService.addProductCost(1L, savedProductCostRequestDto);
+
+        var newProductCostRequestDto = new ProductCostRequestDto("new-paper", 20.00);
+        //When
+        productService.updateProductCost(1L, newProductCostRequestDto);
+
+        //Then
+        var fetchedProductCost = productCostRepository.findById(1L);
+        assertEquals("new-paper", fetchedProductCost.get().getName());
+        assertEquals(20.00, fetchedProductCost.get().getPrice());
+
+        var fetchedProduct = productRepository.findById(1L);
+        assertEquals("new-paper", fetchedProduct.get().getProductCosts().getFirst().getName());
+        assertEquals(20.00, fetchedProduct.get().getProductCosts().getFirst().getPrice());
     }
 }
